e0251c98f189450eff17dcc093d80619151303f9
Tiago Souza <tiago.souza9935@gmail.com>
2021-02-22 14:22:29 -0300

    Primeira Aula

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
 import express from 'express';
 
 const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World - NLW04" });
+});
+
+// 1 param => Rota(Recurso API)
+// 2 param => request, response
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar
+    return response.json({ message: "Os dados foram salvos com sucesso!" });
+});
+
+// 1 param => Porta Utilizada
+app.listen(3333, () => console.log("Server is running!"));
